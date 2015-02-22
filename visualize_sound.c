cd35a9f98b89d40f98032e429166b5c736d488cc
xouabita <xouabita@gmail.com>
2015-02-22 15:54:07 +0100

    transform sound into mono

--- visualize_sound.c
+++ visualize_sound.c
@@ -3,6 +3,23 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// Take sound data with any amounts of channels and return a mono
+// data by doing the average between the two channels
+float * make_mono (float * data, SF_INFO file_info) {
+  float * result = malloc(file_info.frames*sizeof(float));
+  int nb_channels = file_info.channels;
+
+  for (int i=0; i < file_info.frames-nb_channels; i++) {
+    float frame = 0;
+    for (int j=0; j < nb_channels; j++) {
+      frame += data[i*nb_channels+j];
+    }
+    result[i] = frame / (float)nb_channels;
+  }
+
+  return result;
+}
+
 int main (int argc, char * argv[]) {
   SF_INFO file_info;
   if (argc < 2) {
@@ -15,16 +32,19 @@ int main (int argc, char * argv[]) {
   float * samples  = malloc(nb_samples*sizeof(float));
   sf_read_float(file, samples, nb_samples);
 
+  float * mono = make_mono(samples, file_info);
+
   FILE * gp = popen("gnuplot","w");
   fprintf(gp, "set term png\n");
   fprintf(gp, "set out \"waveform.png\"\n");
   fprintf(gp, "plot '-' with lines\n");
 
-  for (int i=0; i < nb_samples; ++i)
+  for (int i=1; i < nb_samples; i+=2)
     fprintf(gp, "%f\n",samples[i]);
   fprintf(gp,"e\n");
 
   free(samples);
+  free(mono);
   sf_close(file);
 
   return 0;
