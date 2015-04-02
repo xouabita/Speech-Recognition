36b4ca1c9e33337596f6098de538b6435e7eb1e0
Alexis Yang <alexis.yang@epita.fr>
2015-04-02 15:04:52 -0700

    signal analysis

--- src/signal_analysis.c
+++ src/signal_analysis.c
@@ -14,4 +14,21 @@ for(int i=0;i<trame;i++)
 	segments[i][j]=module(segment[i][j])*module(segment[i][j]/samplerate;	//periodogram estimate of the power spectrum
 }
 
+float hertz_to_mel (int i , int samplerate){
+	float low_bound = 1125*log(1+20/700);
+	float upper_bound = 1125*log(samplerate/2/700);
+	float gap=(upper_bound-low_bound)/27;
+	return (low_bound+(i*gap)); 
+}
+
+float mel_to_hertz(int samplerate, int i)
+{
+return 700*exp((hertz_to_mel(i,samplerate)/1125)-1);
+}
+
+float fft_bin(int i, int samplerate,int fft_length)
+{
+	return floorf(((fft_length+1)*mel_to_hertz(samplerate,i))/samplerate);
+}
+
 
