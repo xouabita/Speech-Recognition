c48f69566d28e2cdabc1e0ec1e71dcfb7e8d619e
xouabita <xouabita@gmail.com>
2015-05-28 03:27:43 +0200

    add button for new commands

--- gui/app.js
+++ gui/app.js
@@ -1,5 +1,16 @@
+var gui = require('nw.gui');
+
 $(document).ready(function() {
   $('#button').click(function() {
     $('#button').toggleClass('recording');
+  });
+
+  $('#menu').click(function() {
+    if ($('#menu').hasClass('active')) {
+      gui.Window.get().height = 70;
+    } else {
+      gui.Window.get().height = 300;
+    }
+    $('#menu').toggleClass('active');
   });
 });
